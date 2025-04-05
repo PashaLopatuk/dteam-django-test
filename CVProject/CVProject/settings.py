541ff63020f59170f1cfd53984d6bf6a6ff80ff3
Pavlo Lopatiuk <pashalopatuk@gmail.com>
2025-04-05 20:15:49 +0300

    updated allowed host

--- CVProject/CVProject/settings.py
+++ CVProject/CVProject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [
     "dteam-django-test.onrender.com"
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "dteam-django-test.onrender.com"
+    "https://dteam-django-test.onrender.com"
 ]
 
 # Application definition
